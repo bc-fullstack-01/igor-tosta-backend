f3e6d7dde03ce3d16215e1652bafe87db4fc25cf
igorttosta <matositosta@gmail.com>
2022-07-04 12:07:12 -0300

    add routers

--- express-fs-static.js
+++ express-fs-static.js
@@ -3,10 +3,54 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    const content = fs.readFileSync(path.join(__dirname + '/static/index.html'), 'utf-8');
-    res.send(content);
+const bodyParser = require('body-parser')
+const router = express.Router();
+ 
+app.use(bodyParser.json())
+
+// app.get('/', function (req, res) {
+//     const content = fs.readFileSync(path.join(__dirname + '/static/index.html'), 'utf-8');
+//     res.send(content);
+// })
+
+router.route("/").all((req, res, next) => {
+    console.log(`Request from : ${req.originalUrl}`)
+    console.log(`Request type : ${req.method}`)
+    console.log(`Request params : ${req.params}`)
+    next();
+}).get((req, res) => {
+    console.log(req.params)
+    res.send(JSON.stringify([]))
+    res.status(200)
+    res.end()
+}).post((req, res) => {
+    console.log(JSON.stringify(req.body))
+    res.status(201)
+    res.end()
 })
+ 
+router.param('id', (req, res, next, id) => {
+    console.log(`Request from : ${req.params}`)
+    console.log(`Request type : ${req.method}`)
+    console.log(`Request id : ${id}`)
+    next()
+}).route("/:id").get((req, res) => {
+    console.log(req.params)
+    res.send(JSON.stringify({}))
+    res.status(200)
+    res.end()
+}).put((req, res) => {
+    console.log(req.body)
+    res.status(203)
+    res.end()
+}).delete((req, res) => {
+    res.status(203)
+    res.end()
+})
+ 
+ 
+app.use('/posts', router);
+app.use('/', express.static(__dirname + '/static'));
 
 app.listen(4000, () => {
     console.log('server listen on http://localhost:4000');
