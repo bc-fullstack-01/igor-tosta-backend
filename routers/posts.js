4062b7cd109bc6165897c2d0eab9d14e10eae020
igorttosta <matositosta@gmail.com>
2022-07-05 09:55:59 -0300

    add routers past

---
+++ routers/posts.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json())
+
+router.route("/").all((req, res, next) => {
+    console.log(`Request from : ${req.originalUrl}`)
+    console.log(`Request type : ${req.method}`)
+    console.log(`Request params : ${req.params}`)
+    next();
+}).get((req, res) => {
+    console.log(req.params)
+    res.send(JSON.stringify([]))
+    res.status(200)
+    res.end()
+}).post((req, res) => {
+    console.log(JSON.stringify(req.body))
+    res.status(201)
+    res.end()
+})
+ 
+router.param('id', (req, res, next, id) => {
+    console.log(`Request from : ${req.params}`)
+    console.log(`Request type : ${req.method}`)
+    console.log(`Request id : ${id}`)
+    next()
+}).route("/:id").get((req, res) => {
+    console.log(req.params)
+    res.send(JSON.stringify({}))
+    res.status(200)
+    res.end()
+}).put((req, res) => {
+    console.log(req.body)
+    res.status(203)
+    res.end()
+}).delete((req, res) => {
+    res.status(203)
+    res.end()
+})
+
+module.exports = router;
