a2089e94aa945258466952ab592c229247668b81
igorttosta <matositosta@gmail.com>
2022-07-08 09:23:26 -0300

    Corrigindo bugs no MVC

--- mvc/app.js
+++ mvc/app.js
@@ -4,17 +4,15 @@ const methodOverride = require('method-override');
 const createError = require("http-errors");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 
 const index = require("./routers/index")
 const user = require("./routers/userRouters");
-
-
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 
 
 app.use(logger("dev"));
@@ -24,6 +22,23 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
 
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "secret"
+}));
+
+app.use((req,res,next) =>{
+    res.locals = Object.assign(res.locals, req.session.form)
+    res.locals.errors = Object.assign([], res.locals, req.session.errors)
+    res.locals.messages = Object.assign([], res.locals, req.session.messages)
+    
+    next()
+
+    req.session.errors = []
+    req.session.messages = []
+    req.session.form = {}
+});
 
 app.use("/", index);
 app.use("/user", user);
@@ -31,10 +46,21 @@ app.use("/user", user);
 app.use((req,res,next) =>next(createError(404)));
 
 app.use((err, req, res, next) =>{
+
+    if( err.name=== "ValidationError"){
+        const lastView = req.headers.referer.replace(`${req.headers.origin}/`, "/");
+        
+        req.session.form = req.body;
+        req.session.errors = Object.entries(err.errors).map(([,obj])=> obj);
+        req.session.messages.push(err.message);
+
+        res.redirect(lastView);
+    }
     
-    if((err.status === 404) || ( err.name && err.name === "CastError") ){
+    else if((err.status === 404) || ( err.name && err.name === "CastError") ) {
         res.status(404).render("404", {url: req.originalUrl});
-    }else{
+    } 
+    else {
         res.status(500).render("500", {err});
     }
 })
--- mvc/controller/userController.js
+++ mvc/controller/userController.js
@@ -19,22 +19,27 @@ module.exports = {
         .then(data =>{
             res.redirect(`user/${data._id}`)})
         .catch(err => next(err)),
-    
+
     getPost : (req, res, next) => Promise.resolve()
         .then(() => user.findById(req.params.id))
         .then(data => {
-            
-            if (data){
-                res.render("show_users", {user: data})
-            }else{
-                next(createError(404))
-            }
+            res.render("show_users", {user: data})     
         })
         .catch(err => next(err)),
+
     deletePost: (req, res, next) => Promise.resolve()
         .then(() => user.findByIdAndDelete(req.params.id))
         .then(() =>{
             res.redirect("/user")
         })
-        .catch(err => next(err))
+        .catch(err => next(err)),
+
+    updatePost: (req, res, next) => Promise.resolve()
+    .then(() => user.findByIdAndUpdate(req.params.id, req.body.user,{
+        runValidators: true
+    }))
+    .then(() =>{
+        res.redirect(`/user/${req.params.id}`)
+    })
+    .catch(err => next(err))
 };
