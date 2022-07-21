bb2d414a4d30e172d935595e311e9bda57326bdf
igorttosta <matositosta@gmail.com>
2022-07-21 19:11:55 -0300

    Correção de bugs na API Upload

--- api/model/index.js
+++ api/model/index.js
@@ -1,6 +1,6 @@
 const Schema = require('mongoose');
 
-const connect = Schema.connect('mongodb://127.0.0.1:27017/mydb_development');
+const connect = Schema.connect(`${process.env.MONGO_URL || "mongodb://localhost:27017/mydb_development"}`);
 
 exports.Post = require('./Post.js');
 exports.Comments = require('./Comments')
